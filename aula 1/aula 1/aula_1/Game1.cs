e28cf72bf6e6ee51f12bc1d09a5afdcbd9d72c17
Victor Manuel de Barros Garcia <43515539+Victormbg@users.noreply.github.com>
2019-12-13 13:06:05 -0300

    Add files via upload

---
+++ aula 1/aula 1/aula_1/Game1.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace aula_1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        Texture2D ship;
+        Texture2D Space;
+
+        Vector2 position;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            ship = Content.Load<Texture2D>("star-wars");
+            Space = Content.Load<Texture2D>("gfhfg");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                position.X = position.X - 1;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                position.X += 1;
+            } 
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                position.Y -= 1;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                position.Y++;
+            }     
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Space,new Rectangle(0,
+                                                 0,
+                                                 Window.ClientBounds.Width,
+                                                 Window.ClientBounds.Height),
+                                           Color.White);  
+                                                 
+            spriteBatch.Draw(ship,new Rectangle((int)position.X,
+                                                (int)position.Y,
+                                                Window.ClientBounds.Width / 5,
+                                                Window.ClientBounds.Height/5), 
+                                      Color.White);          
+                                                
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
